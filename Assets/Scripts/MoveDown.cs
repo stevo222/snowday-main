90c6949d86028d0c3a888690b608875e168858d4
stevo222 <stephenostler@yahoo.com>
2022-04-22 11:51:01 -0600

    scriptable objects

--- Assets/Scripts/MoveDown.cs
+++ Assets/Scripts/MoveDown.cs
@@ -7,16 +7,24 @@ public class MoveDown : MonoBehaviour
     public float speed = 5.0f;
     private float zDestroy = -12.0f;
     private Rigidbody objectRB;
+    public ParticleSystem fireParticle;
+    private Animator fireAnim;
 
     // Start is called before the first frame update
     void Start()
     {
+
         objectRB = GetComponent<Rigidbody>();
+        fireAnim = GetComponent<Animator>();
+        fireParticle.Play();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        
         objectRB.AddForce(Vector3.forward * -speed);
 
         if (transform.position.z < zDestroy)
