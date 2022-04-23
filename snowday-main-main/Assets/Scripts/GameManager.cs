5cdd62a4ad7f951b1ece89fcd90d04ada60ad407
stevo222 <stephenostler@yahoo.com>
2022-04-23 14:12:38 -0600

    UI update

--- snowday-main-main/Assets/Scripts/GameManager.cs
+++ snowday-main-main/Assets/Scripts/GameManager.cs
@@ -8,7 +8,12 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private int score;
+    private int lives;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI livesText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
 
     // Start is called before the first frame update
     void Start()
@@ -30,7 +35,29 @@ public class GameManager : MonoBehaviour
 
     public void StartGame()
     {
+        isGameActive = true;
         score = 0;
         UpdateScore(0);
+        lives = 4;
+        //UpdateLives(4);
+    }
+
+    //public void UpdateLives (int _lives)
+   // {
+        //lives + _lives;
+       // livesText.text = "Lives: " + lives;
+        
+   // }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
